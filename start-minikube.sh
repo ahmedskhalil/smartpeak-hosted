7085f2efa1c436835961d937e897243b5ab7b5e5
ahmedskhalil <>
2021-11-11 21:40:45 +0100

    hpa w/ scaleup/scaledowm behavior

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 minikube start --hyperv-virtual-switch "smartpeak-area-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
+#minikube start --cpus max -p no-linkerd --namespace sp-no-linkerd --hyperv-virtual-switch "smartpeak-area-switch-no-linkerd" --v=4 --mount --mount-string="$HOME/dev/data:/data"
