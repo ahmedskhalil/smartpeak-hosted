383dd34df2be345a226bf3110e6bb7efd5f9dd64
ahmedskhalil <>
2021-11-21 13:25:23 +0100

    minikube commands

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 minikube start --hyperv-virtual-switch "smartpeak-area-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
 #minikube start --cpus max -p no-linkerd --namespace sp-no-linkerd --hyperv-virtual-switch "smartpeak-area-switch-no-linkerd" --v=4 --mount --mount-string="$HOME/dev/data:/data"
+#minikube start --cpus max -p sp-hosted --namespace sp-hosted-ns --hyperv-virtual-switch "smartpeak-storage-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
+
+#kubectl get -n sp-hosted-ns deploy -o yaml | linkerd inject - | kubectl apply -f -
