4b6fde6ac862777005bb11627eeb8eae3a089fd0
mk <mk@ATP.local>
2021-12-08 10:27:38 +0100

    db

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 minikube start --hyperv-virtual-switch "smartpeak-area-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
 #minikube start --cpus max -p no-linkerd --namespace sp-no-linkerd --hyperv-virtual-switch "smartpeak-area-switch-no-linkerd" --v=4 --mount --mount-string="$HOME/dev/data:/data"
-#minikube start --cpus max -p sp-hosted --namespace sp-hosted-ns --hyperv-virtual-switch "smartpeak-storage-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
+#minikube start --cpus max -p sp-hosted --namespace sp-hosted-ns --hyperv-virtual-switch "smartpeak-storage-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data" --driver=hyperkit
 
-#kubectl get -n sp-hosted-ns deploy -o yaml | linkerd inject - | kubectl apply -f -
+minikube addons enable ingress -p sp-hosted
+minikube addons enable ingress-dns -p sp-hosted
+minikube addons enable dashboard -p sp-hosted
+minikube addons enable metrics-server -p sp-hosted
+
+linkerd check --pre && linkerd install | kubectl apply -f - && linkerd check
+
+kubectl get -n sp-hosted-ns deploy -o yaml | linkerd inject - | kubectl apply -f -
+
+linkerd viz install | kubectl apply -f - && linkerd check
+
+linkerd viz dashboard &
+
+minikube  service smartpeak-hosted -p sp-hosted -n sp-hosted-ns
