f2a620a351b48cd9f2e6d22e8bc62548e48c7fde
Ahmed Khalil <29197392+ahmedskhalil@users.noreply.github.com>
2021-12-31 10:39:02 +0100

    changes

---
+++ helm-install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+helm install --namespace sp-hosted-ns smartpeak-hosted .
--- start-minikube.sh
+++ start-minikube.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 minikube start --hyperv-virtual-switch "smartpeak-area-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data"
 #minikube start --cpus max -p no-linkerd --namespace sp-no-linkerd --hyperv-virtual-switch "smartpeak-area-switch-no-linkerd" --v=4 --mount --mount-string="$HOME/dev/data:/data"
-#minikube start --cpus max -p sp-hosted --namespace sp-hosted-ns --hyperv-virtual-switch "smartpeak-storage-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data" --driver=hyperkit
+#minikube start --cpus max --memory 8192 -p sp-hosted --namespace sp-hosted-ns --hyperv-virtual-switch "smartpeak-storage-switch" --v=4 --mount --mount-string="$HOME/dev/data:/data" --driver=hyperkit
 
 minikube addons enable ingress -p sp-hosted
 minikube addons enable ingress-dns -p sp-hosted
 minikube addons enable dashboard -p sp-hosted
 minikube addons enable metrics-server -p sp-hosted
+
+kubectl create namespace sp-hosted-ns
 
 linkerd check --pre && linkerd install | kubectl apply -f - && linkerd check
 
